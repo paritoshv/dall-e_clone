afbd50c0f469ac30b7d2432b1370f3c4e64b5e01
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-04-15 20:25:59 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -20,7 +20,8 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-3",
       prompt,
       n: 1,
       size: '1024x1024',
